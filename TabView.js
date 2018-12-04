e4e65ff176544bd684a2b49d1ffb0cd8e601cc45
shenweikang <shenweikang@daling.com>
2018-12-04 15:44:45 +0800

    city picker

---
+++ TabView.js
@@ -0,0 +1,311 @@
+import React, { Component, PureComponent } from 'react'
+import {
+    View,
+    Text,
+    Animated,
+    ScrollView,
+    TouchableOpacity,
+    ActivityIndicator,
+    StyleSheet
+} from 'react-native'
+import ScrollAbleTabView from 'react-native-scrollable-tab-view'
+import { px2dp, isIPhoneX, getBottomSpace, deviceWidth, isIos } from 'react-native-style-adaptive'
+
+class Tab extends PureComponent {
+    static defaultProps = {
+        tabStyle: {
+            content: {},
+            text: {},
+            line: {}
+        }
+    }
+    constructor () {
+        super()
+
+        this.state = {
+            tabWidth: px2dp(80),
+            progress: new Animated.Value(px2dp(32))
+        }
+
+        this.touchRefs = []
+        this.list = []
+        this.list_len = 0
+        this.contentOffsetX = 0
+        this.onScrollEnd = this.onScrollEnd.bind(this)
+    }
+
+    render() {
+        const { activeColor, switchTabs, activeTab, tabStyle } = this.props
+
+        return (
+            <View style={ [tabStyles.container, tabStyle.content] }>
+                <ScrollView
+                    ref={ ref => this.scrollView = ref }
+                    style={ { flex: 1 } }
+                    horizontal
+                    showsHorizontalScrollIndicator={ false }
+                    alwaysBounceHorizontal={ false }
+                    onContentSizeChange={ (contentWidth, contentHeight) => { console.log('盾冬视图', contentWidth, contentHeight) } }
+                    onMomentumScrollEnd={ this.onScrollEnd }
+                >
+                    {
+                        switchTabs.map((tab, i) => {
+                            return <TouchableOpacity
+                                ref={ ref => this.touchRefs[i] = ref }
+                                style={ tabStyles.tabJoke }
+                                activeOpacity={ 0.9 }
+                                key={ tab + i }
+                                onPress={ () => this.tabOnPress(i) }
+                                onLayout={ e => this.setListLayout(e.nativeEvent, i) }
+                                removeClippedSubviews={false}
+                            >
+                                <Text style={ [tabStyles.text, tabStyle.text, activeTab == i && { color: activeColor }]}>
+                                    { tab }
+                                </Text>
+                            </TouchableOpacity>
+                        })
+                    }
+                    <Animated.View 
+                        style={[
+                            tabStyles.line, tabStyle.line, { 
+                                backgroundColor: activeColor,
+                                width: this.state.tabWidth,
+                                left: this.state.progress
+                            }
+                        ]}
+                    />
+                </ScrollView>
+            </View>
+
+        )
+    }
+
+    componentDidUpdate () {
+        this.animation()
+        this.setX(this.props.activeTab)
+    }
+
+    componentDidMount () {
+        this.animation()
+        this.setX(this.props.activeTab)
+    }
+
+    setListLayout(e, i) {
+        this.list[i] = e.layout.width
+        this.list_len = this.list.reduce((prev, next) => prev + next, 0)
+    }
+
+    setX(i) {
+        let len = 0
+
+        for (let index = 0; index < i; index++) len += this.list[index]
+
+        if (len > deviceWidth() / 2 && len < this.list_len - deviceWidth() / 2) {
+            this.scrollView.scrollTo({ x: len })
+        } else if (len > this.list_len - deviceWidth() / 2) {
+            this.scrollView.scrollToEnd()
+        } else {
+            this.scrollView.scrollTo({ x: 0 })
+        }
+    }
+
+    animation () {
+        this.state.progress.stopAnimation()
+        this.touchRefs[this.props.activeTab].measure((frameX, frameY, width, height, pageX, pageY) => {
+            this.setState({ tabWidth: width })
+            this.animated = Animated.spring(
+                this.state.progress,
+                {
+                    toValue: pageX + this.contentOffsetX,
+                    velocity: 20,
+                    tension: 10,
+                }
+            ).start()
+        })
+    }
+
+    tabOnPress (i) {
+        this.props.goToPage(i)
+        this.setX(i)
+        this.animation()
+    }
+    
+    onScrollEnd (e) {
+        this.contentOffsetX = e.nativeEvent.contentOffset.x
+    }
+
+    componentWillUnmount () {
+        this.state.progress.stopAnimation()
+    }
+}
+
+const tabStyles = StyleSheet.create({
+    container: {
+        height: px2dp(80),
+        borderBottomColor: '#EEE',
+        borderBottomWidth: 1,
+        position: 'relative',
+        backgroundColor: '#fff'
+    },
+    tabJoke: {
+        height: px2dp(80),
+        marginHorizontal: px2dp(32),
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    line: {
+        position: 'absolute',
+        height: 2,
+        bottom: 0
+    },
+    text: {
+        fontSize: px2dp(28),
+        color: '#252426'
+    }
+})
+
+export default class extends Component {
+    static defaultProps = {
+        listStyle: {
+            content: {},
+            text: {}
+        },
+        activeColor: '#e4393c'
+    }
+    
+
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            page: props.initPage,
+            address: props.address,
+            tabs: props.tabs,
+            loading: false,
+            tags: props.tags || []
+        }
+    }
+
+    selected (item, index, i) {
+        let newTabs = this.state.tabs
+        let tags = this.state.tags.splice(0, index)
+
+        newTabs[this.state.page] = typeof item === 'object' ? item.name : item
+        
+        tags[index] = i
+        
+        this.setState({ tabs: newTabs, loading: true, tags }, () => {
+            this.props.selected(item, index + 1, newTabs)
+        })
+    }
+
+    _renderOptions (index) {
+        const { listStyle, activeColor } = this.props
+        const address = this.state.address[index] || []
+
+        return address.map((item, i) => <TouchableOpacity 
+            style={ [styles.list, listStyle.content] }
+            key={ i + index }
+            activeOpacity={ 1 }
+            onPress={ () => this.selected(item, index, i) }
+        >
+            <Text style={ [styles.listText, listStyle.text, this.state.tags[index] === i && { color: activeColor }] }>
+                { typeof item === 'string' ? item : item.name }
+            </Text>
+        </TouchableOpacity>)
+    }
+
+    _renderTabView () {
+        return this.state.tabs.map((item, index) => <ScrollView
+            style={ styles.content }
+            showsVerticalScrollIndicator={ false }
+            key={ index }
+            tabLabel={ item || this.props.prompt }
+        >
+            { this._renderOptions(index) }
+        </ScrollView>)
+    }
+
+    setNativeProps (singleAddress) {
+        let newTabs = []
+        let newAddress = []
+
+        const { page, tabs, address } = this.state
+
+        if (page < tabs.length - 1) {
+            newTabs = tabs.splice(0, page + 1)
+            newAddress = address.splice(0, page + 1)
+        } else {
+            newTabs = tabs
+            newAddress = address
+        }
+
+        this.setState({
+            address: [...newAddress, singleAddress],
+            tabs: [...newTabs, this.props.prompt],
+            page: page + 1,
+            loading: false
+        })
+    }
+
+    render () {
+        const { page, tabs } = this.state
+        const { style, initPage, tabStyle, activeColor } = this.props
+        return (
+            <View style={ [styles.container, style] }>
+                <ScrollAbleTabView
+                    style={ styles.main }
+                    locked
+                    initialPage={ isIos ? initPage : page }
+                    renderTabBar={ () => <Tab
+                        activeTab={ page }
+                        switchTabs={ tabs }
+                        tabStyle={ tabStyle }
+                        activeColor={ activeColor }
+                    /> }
+                    page={ page }
+                    onChangeTab={ ({ i }) => this.setState({ page: i }) }
+                >
+                    { this._renderTabView() }
+                </ScrollAbleTabView>
+                {
+                    this.state.loading && <View style={ styles.loading }>
+                        { this.props.loading && <ActivityIndicator size="large" color="#ccc" /> }
+                    </View>
+                }
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        position: 'relative',
+        paddingBottom: isIPhoneX ? getBottomSpace() : 0
+    },
+    main: {
+        flex: 1
+    },
+    content: {
+        paddingHorizontal: px2dp(32),
+        marginVertical: px2dp(10)
+    },
+    list: {
+        paddingVertical: px2dp(33)
+    },
+    loading: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        backgroundColor: 'rgba(255, 255, 255, 0)',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    listText: {
+        color: '#333',
+        fontSize: px2dp(28)
+    }
+})
---
+++ index.js
@@ -0,0 +1,290 @@
+import React, { Component } from 'react'
+import {
+    View,
+    Text,
+    Image,
+    Modal,
+    Animated,
+    TouchableOpacity,
+    ActivityIndicator,
+    TouchableWithoutFeedback,
+    StyleSheet
+} from 'react-native'
+
+import { deviceWidth, deviceHeight, px2dp } from 'react-native-style-adaptive'
+
+import TabView from './TabView'
+
+const Icon = require('./icon-close-base')
+
+export default class extends Component {
+    static defaultProps = {
+        prompt: '请选择',
+        titleStyle: {
+            content: {},
+            text: {}
+        },
+        loading: true
+    }
+    
+    constructor (props) {
+        super(props)
+
+        this.initH = parseInt(deviceHeight() * 4 / 6)
+
+        this.state = {
+            progress: new Animated.Value(this.initH),
+            visible: false,
+            initPage: 0,
+            address: [],
+            tabs: [],
+            tags: []
+        }
+
+        this.timer = null
+        this.onBackClicked = this.onBackClicked.bind(this)
+        this.setAddress = this.setAddress.bind(this)
+    }
+
+    async load () {
+        let tabs = this.props.tabs.length ? this.props.tabs : [this.props.prompt]
+        let address = new Array(tabs.length)
+        let tags = this.state.tags
+
+        await tabs.reduce(async (prev, next, index) => {
+            if (!prev && !!index) return null
+            
+            const eachAddress = await this.getAddress(await prev, index)
+            const result = await this._than(eachAddress, next)
+            const tag = parseInt(result.key)
+
+            address[index] = eachAddress
+            tags[index] = isNaN(tag) ? '' : tag
+
+            return result.option
+        }, null)
+
+        this.setState({ address, tags, tabs, initPage: tabs.length - 1 })
+    }
+
+    _than (data, target) {
+        if (!target && target != 0) return { option: null, key: '' }
+
+        if (Array.isArray(data)) {
+            for (let key of Object.keys(data)) {
+                if (typeof target === 'object') {
+                    if (data[key] === target) {
+                        return { option: data[key], key }
+                    } else {
+                        return this._deepComparison(data[key], target)
+                    }
+                } else {
+                    if (data[key] === target) return { option: data[key], key }
+
+                    if (typeof data[key] === 'object') {
+                        for (let keys of Object.keys(data[key])) {
+                            if (data[key][keys] === target) {
+                                return { option: data[key], key }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return { option: null, key: '' }
+    }
+
+    _deepComparison (data, target) {
+        if (Object.prototype.toString(data) !== Object.prototype.toString(target)) return { option: null, key: '' }
+
+        for (let key of Object.keys(data)) {
+            if (data[key] === target[key]) return { option: data[key], key }
+        }
+
+        return { option: null, key: '' }
+    }
+
+    async getAddress (target, index, tabs) {
+        const address = await this.props.load(target, index)
+
+        if (!address) {
+            this.setState({ tabs, initPage: tabs.length - 1 })
+            this.props.result(tabs)
+            this.close()
+
+            return false
+        }
+
+        if (!Array.isArray(address)) throw Error(`The load method expects an Array type value, but it is actually an ${Object.prototype.toString.call(address).match(/^\[object (.*?)\]$/)[1] } type value`)
+
+        return address
+    }
+
+    async setAddress (target, index, tabs) {
+
+        const address = await this.getAddress(target, index, tabs)
+
+        if (!address) return false
+        
+        await this.tabView.setNativeProps(address)
+    }
+    
+    render () {
+        const { visible, progress, initPage, address, tags, tabs } = this.state
+        const { titleStyle, contentStyle, prompt, listStyle, tabStyle, activeColor, loading } = this.props
+
+        return (
+            <Modal
+                animationType="none"
+                transparent={ true }
+                visible={ visible }
+                onRequestClose={ () => this.onBackClicked.bind(this) }
+                style={ styles.container }
+            >
+                <TouchableWithoutFeedback
+                    onPress={ () => this.close() }
+                >
+                    <Animated.View
+                        style={ [styles.mask, {
+                            opacity: progress.interpolate({
+                                inputRange:[0, this.initH],
+                                outputRange:[0.7, 0]
+                            })
+                        }] }
+                    />
+                </TouchableWithoutFeedback>
+                <Animated.View style={ [styles.content, {
+                    transform: [
+                        { translateY: progress }
+                    ]
+                }] }>
+                    <View style={ [styles.titleMain, titleStyle.content] }>
+                        <Text style={ [styles.title, titleStyle.text] }>配送至</Text>
+                        <TouchableOpacity style={ styles.icons } activeOpacity={0.8} onPress={() => this.close()}>
+                            <Image
+                                style={ styles.close }
+                                source={ { uri: Icon } }
+                            />
+                        </TouchableOpacity>
+                    </View>
+                    { !tabs.length ? 
+                        <View style={ [styles.tabView, contentStyle] }>
+                            <ActivityIndicator size="large" color="#ccc" />
+                        </View>
+                        : <TabView
+                            style={ [styles.tabView, contentStyle] }
+                            ref={ ref => this.tabView = ref }
+                            initPage={ initPage }
+                            address={ address }
+                            tags={ tags }
+                            selected={ this.setAddress }
+                            tabs={ tabs }
+                            prompt={ prompt }
+                            listStyle={ listStyle }
+                            tabStyle={ tabStyle }
+                            activeColor={ activeColor }
+                            loading={ loading }
+                        />}
+                </Animated.View>
+            </Modal>
+        )
+    }
+
+    onBackClicked () {
+        this.close()
+    }
+
+    open () {
+        this.load()
+        this.setState({ visible: true }, () => {
+            Animated.spring(
+                this.state.progress,
+                {
+                    toValue: 0,
+                    velocity: 40,
+                    tension: 10,
+                }
+            ).start()
+        })
+    }
+
+    close () {
+        Animated.spring(
+            this.state.progress,
+            {
+                toValue: this.initH,
+                velocity: 10,
+                tension: 30,  
+            }
+        ).start()
+
+        this.timer = setTimeout(() => {
+            clearTimeout(this.timer)
+            this.setState({ visible: false, tabs: [] })
+        }, 1000)
+    }
+
+    componentWillUnmount () {
+        this.state.visible && this.close()
+        clearTimeout(this.timer)
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        position: 'relative'
+    },
+    mask: {
+        flex: 1,
+        backgroundColor: '#000',
+        opacity: 0
+    },
+    content: {
+        position: 'absolute',
+        left: 0,
+        bottom: 0,
+        width: deviceWidth(),
+        height: parseInt(deviceHeight() * 4 / 6),
+    },
+    tabView: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    titleMain: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        height: px2dp(80),
+        backgroundColor: '#fff',
+        borderBottomColor: '#EEE',
+        borderBottomWidth: 1,
+        position: 'relative'
+    },
+    title: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        flex: 1,
+        fontSize: px2dp(30),
+        color: '#222',
+        textAlign: 'center'
+    },
+    icons: {
+        position: 'absolute',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: px2dp(80),
+        top: 0,
+        bottom: 0,
+        right: 0,
+        zIndex: 999
+    },
+    close: {
+        width: px2dp(21),
+        height: px2dp(21)
+    }
+})
